23f4ad75b0beca1f44e456bcf12bb1bacf07c6c5
sudodot <haikhalroy@gmail.com>
2025-01-07 10:22:53 +0700

    feat: added form and database model for said form

--- home/models.py
+++ home/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
-# Create your models here.
+
+class Peserta(models.Model):
+    GENDER_CHOICES = [("laki laki", "Laki Laki"), ("perempuan", "Perempuan")]
+    COUNTRY_CHOICES = [
+        ("indonesia", "Indonesia"),
+        ("malaysia", "Malaysia"),
+        ("singapura", "Singapura"),
+        ("lainya", "Lainya"),
+    ]
+    BLOOD_CHOICES = [
+        ("a", "A"),
+        ("b", "B"),
+        ("ab", "AB"),
+        ("o", "O"),
+        ("tidak diketahui", "Tidak Diketahui"),
+    ]
+    ID_CHOICES = [
+        ("ktp", "KTP"),
+        ("sim", "SIM"),
+        ("kartu pelajar", "Kartu Pelajar"),
+        ("passport", "Passport"),
+    ]
+    CATEGORY_CHOICES = [
+        ("umum", "Umum (dibawah 45 tahun)"),
+        ("master", "Master (diatas 45 tahun)"),
+    ]
+    SIZE_CHOICES = [("s", "S"), ("m", "M"), ("l", "L"), ("xl", "XL"), ("xxl", "XXL")]
+    YES_NO_CHOICES = [(1, "Ya"), (2, "Tidak")]
+
+    # informasi peserta
+    id_peserta = models.CharField(max_length=4, unique=True, editable=False)
+    nama = models.CharField(max_length=50)
+    email = models.EmailField(max_length=80)
+    jenis_kelamin = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    tempat_lahir = models.CharField(max_length=50)
+    birthday = models.DateField()
+    phone = models.CharField(max_length=12)
+
+    # alamat
+    alamat = models.CharField(max_length=80)
+    kota = models.CharField(max_length=80)
+    provinsi = models.CharField(max_length=80)
+    warga_negara = models.CharField(max_length=20, choices=COUNTRY_CHOICES)
+
+    # identitas
+    gol_darah = models.CharField(max_length=15, choices=BLOOD_CHOICES)
+    jenis_id = models.CharField(max_length=15, choices=ID_CHOICES)
+    no_id = models.CharField(max_length=80)
+    f_id = models.FileField(upload_to="ids/")
+    kategori = models.CharField(max_length=45, choices=CATEGORY_CHOICES)
+
+    # kontak darurat
+    kontak_darurat = models.CharField(max_length=80)
+    no_kontak_darurat = models.CharField(max_length=80)
+
+    # riwayat kesehatan
+    riwayat_penyakit_jantung = models.IntegerField(choices=YES_NO_CHOICES)
+    riwayat_penyakit_hipertensi = models.IntegerField(choices=YES_NO_CHOICES)
+    riwayat_penyakit_kronik = models.IntegerField(choices=YES_NO_CHOICES)
+    riwayat_penyakit_epilepsi = models.IntegerField(choices=YES_NO_CHOICES)
+    riwayat_penyakit_asma = models.IntegerField(choices=YES_NO_CHOICES)
+    memiliki_asuransi = models.IntegerField(choices=YES_NO_CHOICES)
+    riwayat_alergi_obat = models.IntegerField(choices=YES_NO_CHOICES)
+
+    # info tambahan
+    alergi = models.CharField(max_length=80, blank=True)
+    bib = models.CharField(max_length=80)
+    size_baju = models.CharField(max_length=5, choices=SIZE_CHOICES)
+    f_pembayaran = models.FileField(upload_to="payments/")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nama
+
+    def save(self, *args, **kwargs):
+        if not self.id_peserta:
+            used_ids = set(Peserta.objects.values_list("id_peserta", flat=True))
+            for i in range(1, 10000):
+                candidate = str(i).zfill(4)
+                if candidate not in used_ids:
+                    self.id_peserta = candidate
+                    break
+        super().save(*args, **kwargs)
--- home/views.py
+++ home/views.py
@@ -1,4 +1,55 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+
+from .forms import Peserta as PesertaForm
+from .models import Peserta
+
 
 def home(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        form = PesertaForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Create Peserta instance from form data
+            peserta = Peserta(
+                nama=form.cleaned_data["nama"],
+                email=form.cleaned_data["email"],
+                jenis_kelamin=form.cleaned_data["jenis_kelamin"],
+                tempat_lahir=form.cleaned_data["tempat_lahir"],
+                birthday=form.cleaned_data["birthday"],
+                phone=form.cleaned_data["phone"],
+                alamat=form.cleaned_data["alamat"],
+                kota=form.cleaned_data["kota"],
+                provinsi=form.cleaned_data["provinsi"],
+                warga_negara=form.cleaned_data["warga_negara"],
+                gol_darah=form.cleaned_data["gol_darah"],
+                jenis_id=form.cleaned_data["jenis_id"],
+                no_id=form.cleaned_data["no_id"],
+                f_id=form.cleaned_data["f_id"],
+                kategori=form.cleaned_data["kategori"],
+                kontak_darurat=form.cleaned_data["kontak_darurat"],
+                no_kontak_darurat=form.cleaned_data["no_kontak_darurat"],
+                riwayat_penyakit_jantung=form.cleaned_data["riwayat_penyakit_jantung"],
+                riwayat_penyakit_hipertensi=form.cleaned_data[
+                    "riwayat_penyakit_hipertensi"
+                ],
+                riwayat_penyakit_kronik=form.cleaned_data["riwayat_penyakit_kronik"],
+                riwayat_penyakit_epilepsi=form.cleaned_data[
+                    "riwayat_penyakit_epilepsi"
+                ],
+                riwayat_penyakit_asma=form.cleaned_data["riwayat_penyakit_asma"],
+                memiliki_asuransi=form.cleaned_data["memiliki_asuransi"],
+                riwayat_alergi_obat=form.cleaned_data["riwayat_alergi_obat"],
+                alergi=form.cleaned_data["alergi"],
+                bib=form.cleaned_data["bib"],
+                size_baju=form.cleaned_data["size_baju"],
+                f_pembayaran=form.cleaned_data["f_pembayaran"],
+            )
+            peserta.save()
+            messages.success(request, "Pendaftaran berhasil!")
+            return redirect("home")
+        else:
+            messages.error(request, "Mohon periksa kembali form anda.")
+    else:
+        form = PesertaForm()
+
+    return render(request, "home.html", {"form": form})
